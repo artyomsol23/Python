b75a9627ad555f22b9c2e81fd394d16e33910c4a
Artem S. <artyom.sol23@icloud.com>
2025-06-27 15:42:50 +0300

    Update transpose_matrix.py

--- matrix/transpose_matrix.py
+++ matrix/transpose_matrix.py
@@ -1,12 +1,10 @@
 def transpose_matrix(matrix):
+    """
+    Транспонирует матрицу (заменяет строки на столбцы).
+    """
     rows = len(matrix)
     cols = len(matrix[0]) if rows > 0 else 0
     
-    transposed = [[0 for _ in range(rows)] for _ in range(cols)]
-    
-    for i in range(rows):
-        for j in range(cols):
-            transposed[j][i] = matrix[i][j]
+    transposed = [[matrix[j][i] for j in range(rows)] for i in range(cols)]
     
     return transposed
-  
