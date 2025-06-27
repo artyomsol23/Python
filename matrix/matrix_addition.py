2ab46269ec24b15ea75fce648fc99d99d993493d
Artem S. <artyom.sol23@icloud.com>
2025-06-27 14:39:37 +0300

    Create matrix_addition.py

---
+++ matrix/matrix_addition.py
@@ -0,0 +1,19 @@
+def matrix_addition(A, B):
+    """
+    Сложение двух матриц A и B одинакового размера.
+    Возвращает новую матрицу C = A + B.
+    """
+    if len(A) != len(B) or len(A[0]) != len(B[0]):
+        raise ValueError("Матрицы должны быть одного размера!")
+    
+    rows = len(A)
+    cols = len(A[0])
+    
+    C = [[0 for _ in range(cols)] for _ in range(rows)]
+    
+    for i in range(rows):
+        for j in range(cols):
+            C[i][j] = A[i][j] + B[i][j]
+    
+    return C
+  
