cc44162731ce02dc5bd4342a2ba78f0b358e3103
Artem S. <artyom.sol23@icloud.com>
2025-06-27 13:34:10 +0300

    Create build_shifted_matrix.py

---
+++ def/build_shifted_matrix.py
@@ -0,0 +1,9 @@
+def build_shifted_matrix(rows, cols, direction="left"):
+    """
+    Создаёт матрицу rows × cols с циклическим сдвигом в каждой строке.
+    """
+    matrix = []
+    for i in range(rows):
+        shifted_row = [(j + i) % cols + 1 for j in range(cols)]
+        matrix.append(shifted_row)
+    return matrix
