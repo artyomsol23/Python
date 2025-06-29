71b3f73c206d303e1d545eb9a25fa5a8fa00c7d3
Artem S. <artyom.sol23@icloud.com>
2025-06-29 12:49:09 +0300

    Update chebyshev_distance.py

--- distance/chebyshev_distance.py
+++ distance/chebyshev_distance.py
@@ -1,5 +1,5 @@
 def chebyshev_distance(a: tuple[float, float], b: tuple[float, float]):
-    dx = abs(a[0] - b[0])  # Разность по оси X
-    dy = abs(a[1] - b[1])  # Разность по оси Y
+    dx = abs(a[0] - b[0])
+    dy = abs(a[1] - b[1])
     
     return max(dx, dy)
