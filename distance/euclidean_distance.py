288949ed3896f0490a130480f6c39e5f662f7899
Artem S. <artyom.sol23@icloud.com>
2025-06-29 12:49:22 +0300

    Update euclidean_distance.py

--- distance/euclidean_distance.py
+++ distance/euclidean_distance.py
@@ -2,6 +2,4 @@ def euclidean_distance(a: tuple[float, float], b: tuple[float, float]):
     dx = a[0] - b[0]
     dy = a[1] - b[1]
     
-    distance = (dx ** 2 + dy ** 2) ** 0.5
-    
-    return distance
+    return (dx ** 2 + dy ** 2) ** 0.5
