1a8e85d21bf5680e8b7feda2d0243c459f7eb6f8
Artem S. <artyom.sol23@icloud.com>
2025-06-29 12:33:41 +0300

    Update caesar_cipher.py

--- def/caesar_cipher.py
+++ def/caesar_cipher.py
@@ -1,15 +1,7 @@
 def caesar_cipher(text, shift, mode='encrypt'):
-    """
-    Функция для шифрования и дешифрования текста с использованием шифра Цезаря.
-    
-    :param text: Исходный текст для преобразования (str)
-    :param shift: Величина сдвига алфавита (int)
-    :param mode: Режим работы: 'encrypt' - шифрование, 'decrypt' - дешифрование (str)
-    :return: Преобразованный текст (str)
-    """
     result = ""
     
-    if mode == 'decrypt':
+    if mode == 'decrypt':  # 'encrypt' - шифрование, 'decrypt' - дешифрование
         shift = -shift
     
     for char in text:
