ac1bb1583546379bbfb7e2a5a1cf485a29f58328
Artem S. <artyom.sol23@icloud.com>
2025-07-05 11:59:59 +0300

    Update square_matrix_power.py

--- matrix/square_matrix_power.py
+++ matrix/square_matrix_power.py
@@ -1,4 +1,4 @@
-def square_matrix_power(A: list[list[int]], power: int) -> int:
+def square_matrix_power(A: list[list[int]], power: int) -> list[list[int]]:
     if len(A) != len(A[0]):
         raise ValueError("Матрица должна быть квадратной!")
     
