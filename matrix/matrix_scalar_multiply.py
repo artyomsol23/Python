7eca76389d5748f2a7d7fd8c8bf97b6ffe009651
Artem S. <artyom.sol23@icloud.com>
2025-07-05 11:56:52 +0300

    Update matrix_scalar_multiply.py

--- matrix/matrix_scalar_multiply.py
+++ matrix/matrix_scalar_multiply.py
@@ -1,4 +1,4 @@
-def matrix_scalar_multiply(A: list[list[int]], c: float):
+def matrix_scalar_multiply(A: list[list[int]], c: float) -> float:
     result = [[0.0 for _ in range(len(A[0]))] for _ in range(len(A))]
     
     for i in range(len(A)):
