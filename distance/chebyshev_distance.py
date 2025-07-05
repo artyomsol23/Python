3d625bbbb80cad634d0d08f9d1465670fb6e7a2a
Artem S. <artyom.sol23@icloud.com>
2025-07-05 11:50:51 +0300

    Update chebyshev_distance.py

--- distance/chebyshev_distance.py
+++ distance/chebyshev_distance.py
@@ -1,4 +1,4 @@
-def chebyshev_distance(a: tuple[float, float], b: tuple[float, float]):
+def chebyshev_distance(a: tuple[float, float], b: tuple[float, float]) -> tuple[float, float]:
     dx = abs(a[0] - b[0])
     dy = abs(a[1] - b[1])
     
