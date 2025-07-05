faecf4da463cabe3a169f50c4550eaf53bfb8457
Artem S. <artyom.sol23@icloud.com>
2025-07-05 12:05:07 +0300

    Update linear_search.py

--- search/linear_search.py
+++ search/linear_search.py
@@ -1,4 +1,4 @@
-def linear_search(arr: list[list[int]], target: int):
+def linear_search(arr: list[list[int]], target: int) -> int:
     for i in range(len(arr)):
         if arr[i] == target:
             return i
