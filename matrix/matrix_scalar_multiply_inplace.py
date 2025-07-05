ebf77c00682ed48d6e39fd78597a2c17c268f48a
Artem S. <artyom.sol23@icloud.com>
2025-07-05 11:57:02 +0300

    Update matrix_scalar_multiply_inplace.py

--- matrix/matrix_scalar_multiply_inplace.py
+++ matrix/matrix_scalar_multiply_inplace.py
@@ -1,4 +1,4 @@
-def matrix_scalar_multiply_inplace(A: list[list[int]], c: float):
+def matrix_scalar_multiply_inplace(A: list[list[int]], c: float) -> float:
     for i in range(len(A)):
         for j in range(len(A[0])):
             A[i][j] *= c
