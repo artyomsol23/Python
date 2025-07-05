a6ec0e609f815626b2949db68ea8f24f1c3b859f
Artem S. <artyom.sol23@icloud.com>
2025-07-05 11:51:48 +0300

    Update euclidean_distance_sq.py

--- distance/euclidean_distance_sq.py
+++ distance/euclidean_distance_sq.py
@@ -1,4 +1,4 @@
-def euclidean_distance_sq(a: tuple[float, float], b: tuple[float, float]):
+def euclidean_distance_sq(a: tuple[float, float], b: tuple[float, float]) -> float:
     dx_sq = (a[0] - b[0]) ** 2    
     dy_sq = (a[1] - b[1]) ** 2
     
