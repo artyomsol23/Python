8ba5c093b55beaa35d87d91f24e4fcef32b68980
Artem S. <artyom.sol23@icloud.com>
2025-07-05 11:53:07 +0300

    Update queen_distance.py

--- distance/queen_distance.py
+++ distance/queen_distance.py
@@ -1,4 +1,4 @@
-def queen_distance(a: tuple[int, int], b: tuple[int, int]):
+def queen_distance(a: tuple[int, int], b: tuple[int, int]) -> int:
     dx = abs(a[0] - b[0])
     dy = abs(a[1] - b[1])
     
