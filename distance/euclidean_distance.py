89a761dc4737f6249fd9238fd0eb1505c267a267
Artem S. <artyom.sol23@icloud.com>
2025-07-05 11:51:38 +0300

    Update euclidean_distance.py

--- distance/euclidean_distance.py
+++ distance/euclidean_distance.py
@@ -1,4 +1,4 @@
-def euclidean_distance(a: tuple[float, float], b: tuple[float, float]) -> tuple[float, float]:
+def euclidean_distance(a: tuple[float, float], b: tuple[float, float]) -> float:
     dx = a[0] - b[0]
     dy = a[1] - b[1]
     
