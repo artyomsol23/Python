f222804a2bf66a7ef420f777cafcc488ececdae3
Artem S. <artyom.sol23@icloud.com>
2025-07-21 12:40:44 +0300

    Create gini.py

---
+++ ml/gini.py
@@ -0,0 +1,20 @@
+def gini(labels):
+    # Подсчет количества каждого уникального класса
+    classes = {}
+    for label in labels:
+        if label not in classes:
+            classes[label] = 0
+        classes[label] += 1
+
+    total = len(labels)
+    if total == 0:
+        return 0
+    
+    sum_squares = 0.0
+    for count in classes.values():
+        proportion = count / total
+        sum_squares += proportion ** 2
+    
+    gini = 1 - sum_squares
+    return gini
+  
