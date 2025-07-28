8dbbfc944786e8f88804818d6777dd5782aa5740
Artem S. <artyom.sol23@icloud.com>
2025-07-28 18:11:56 +0300

    Create logistic_regression_margin_probability.py

---
+++ ml/logistic_regression_margin_probability.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def logistic_regression_margin_probability(w: np.ndarray, x: np.ndarray) -> tuple[float, float]:
+    margin = np.dot(w, x)
+    probability = 1 / (1 + np.exp(-margin))
+    return round(margin, 2), round(probability, 2)
+  
