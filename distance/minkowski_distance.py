54326dbc2eaa283823bdc22fd3ee8ed3fcb510ce
Artem S. <artyom.sol23@icloud.com>
2025-08-01 13:02:58 +0300

    Update minkowski_distance.py

--- distance/minkowski_distance.py
+++ distance/minkowski_distance.py
@@ -1,2 +1,4 @@
-def minkowski_distance(a: np.array, b: np.array, p=2) -> float:  # k-NN с метрикой Минковского
-    return np.sum(np.abs(a - b) ** p) ** (1 / p)                 # по умолчанию Евклидово расстояние
+# k-NN с метрикой Минковского
+def minkowski_distance(a: np.array, b: np.array, p=2) -> float:  # по умолчанию Евклидово расстояние
+    return np.sum(np.abs(a - b) ** p) ** (1 / p)                 # np.sum(np.abs(a - b) ** p, axis=-1) ** (1 / p), если
+                                                                 # необходимо суммирование по последней оси (признакам)
