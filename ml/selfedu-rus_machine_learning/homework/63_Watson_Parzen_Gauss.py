5cc8fc54ce843b6a6aaae94bbf3570bfe468ceeb
Artem S. <artyom.sol23@icloud.com>
2025-08-01 16:25:18 +0300

    Update 63_Watson_Parzen_Gauss.py

--- ml/selfedu-rus_machine_learning/homework/63_Watson_Parzen_Gauss.py
+++ ml/selfedu-rus_machine_learning/homework/63_Watson_Parzen_Gauss.py
@@ -13,8 +13,8 @@ y = func(x) + np.random.normal(0, 0.2, len(x)) # значения функции
 # здесь продолжайте программу
 h = 1.0
 
-K = lambda r: np.exp(-2 * r * r)
-ro = lambda xx, xi: np.abs(xx - xi)
+K = lambda r: np.exp(-2 * r * r)     # гауссовское парзеновское окно
+ro = lambda xx, xi: np.abs(xx - xi)  # модуль разностей
 w = lambda xx, xi: K(ro(xx, xi) / h)
 
 y_est = []
