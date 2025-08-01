8d2f34eed1eacdd88ece571b9e960de215bee76c
Artem S. <artyom.sol23@icloud.com>
2025-08-01 13:28:11 +0300

    Create euclidean_distance_matrix_NumPy.py

---
+++ distance/euclidean_distance_matrix_NumPy.py
@@ -0,0 +1,5 @@
+def euclidean_distance_matrix(X, Y):
+    X_sq = np.sum(X ** 2, axis=1, keepdims=True)
+    Y_sq = np.sum(Y ** 2, axis=1)
+    XY = np.dot(X, Y.T)
+    return np.sqrt(X_sq + Y_sq - 2 * XY)
