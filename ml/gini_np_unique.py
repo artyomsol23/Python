76ec1a4dee069e140ee0f8df3b3df65bc121c076
Artem S. <artyom.sol23@icloud.com>
2025-08-02 19:31:05 +0300

    Update gini_np_unique.py

--- ml/gini_np_unique.py
+++ ml/gini_np_unique.py
@@ -4,6 +4,6 @@ def gini(y: np.ndarray) -> float:
     if len(y) == 0:
         return 0.0
     _, counts = np.unique(y, return_counts=True)
-    proportions = counts / len(y)
-    return 1 - np.sum(proportions ** 2)
+    p = counts / len(y)
+    return 1 - np.sum(p ** 2)
   
