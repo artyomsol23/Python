77c7544985a829dfba904b620b1fc769684d36be
Artem S. <artyom.sol23@icloud.com>
2025-08-02 19:43:57 +0300

    Update logistic_regression_probability.py

--- ml/logistic_regression_probability.py
+++ ml/logistic_regression_probability.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def logistic_regression_probability(w: np.ndarray, x: np.ndarray) -> float:
-    a_x = np.dot(w, x)  # линейная комбинация w^T * x
+    a_x = np.dot(w, x)                   # линейная комбинация w^T * x
     probability = 1 / (1 + np.exp(a_x))  # cигмоидная функция
+    
     return round(probability, 2)
