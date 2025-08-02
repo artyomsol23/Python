9c5641e4dbbecaa0afd23395c147fa887159e0b5
Artem S. <artyom.sol23@icloud.com>
2025-08-02 19:40:20 +0300

    Update calculate_mse.py

--- ml/calculate_mse.py
+++ ml/calculate_mse.py
@@ -3,11 +3,10 @@ def loss(x: float, y: float, w: list) -> float:
 
 def calculate_mse(coord_x: list, coord_y: list, w: list) -> float:
     total_loss = 0.0
-    sz = len(coord_x)
     
-    for i in range(sz):
+    for i in range(len(coord_x)):
         total_loss += loss(coord_x[i], coord_y[i], w)
     
-    return total_loss / sz  # mean squared error
+    return total_loss / len(coord_x)  # mean squared error
 
 Q = calculate_mse(coord_x, coord_y, w)
