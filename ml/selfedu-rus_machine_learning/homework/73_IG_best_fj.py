9c7a6afe53e5982f760e497ef29d9c5ad32d593a
Artem S. <artyom.sol23@icloud.com>
2025-08-02 21:11:51 +0300

    Update 73_IG_best_fj.py

--- ml/selfedu-rus_machine_learning/homework/73_IG_best_fj.py
+++ ml/selfedu-rus_machine_learning/homework/73_IG_best_fj.py
@@ -29,7 +29,6 @@ for j in [0, 1]:
         right_i = np.where(x_train[:, j] > t)[0] 
         y1, y2 = y_train[left_i], y_train[right_i]
         N1, N2 = len(y1), len(y2)
-        N0 = N1 + N2
 
         if N1 == 0 or N2 == 0:
             continue
