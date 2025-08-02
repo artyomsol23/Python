953fce351e7e3fa22a69a9bfa8d5a97853e272e0
Artem S. <artyom.sol23@icloud.com>
2025-08-02 19:41:55 +0300

    Update compute_covariance_matrix.py

--- ml/compute_covariance_matrix.py
+++ ml/compute_covariance_matrix.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 def compute_covariance_matrix(samples: np.ndarray, means: np.ndarray) -> np.ndarray:
-    num_samples = len(samples)
     VV_total = np.zeros((3, 3))
     N = samples[0].shape[1]
     
@@ -12,6 +11,7 @@ def compute_covariance_matrix(samples: np.ndarray, means: np.ndarray) -> np.ndar
             [np.dot(a[1], a[0]) / N, np.dot(a[1], a[1]) / N, np.dot(a[1], a[2]) / N],
             [np.dot(a[2], a[0]) / N, np.dot(a[2], a[1]) / N, np.dot(a[2], a[2]) / N]
         ])
+        
         VV_total += VV
     
-    return VV_total / num_samples
+    return VV_total / len(samples)
