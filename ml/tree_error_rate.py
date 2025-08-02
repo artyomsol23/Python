454c0c278eb57eae1ff3bd4d9e666c16870a42d0
Artem S. <artyom.sol23@icloud.com>
2025-08-02 19:45:42 +0300

    Update tree_error_rate.py

--- ml/tree_error_rate.py
+++ ml/tree_error_rate.py
@@ -6,5 +6,5 @@ def tree_error_rate(y_true: list, y_pred: list) -> float:
         return 0.0
     
     errors = sum(1 for true, pred in zip(y_true, y_pred) if true != pred)
+    
     return errors / len(y_true)
-  
