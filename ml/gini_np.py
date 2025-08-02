987aaee4acdf228663dcdc5725753eede15fbaa5
Artem S. <artyom.sol23@icloud.com>
2025-08-02 18:52:36 +0300

    Update gini_np.py

--- ml/gini_np.py
+++ ml/gini_np.py
@@ -2,6 +2,9 @@ import numpy as np
 
 def gini(y: np.ndarray) -> float:
     if len(y) == 0:
-        return 0.0
+        return 0.0      
+    # Для преобразования {-1, 1} в {0, 1}:
+    # y_transf = (y == 1)
+    # proportions = np.bincount(y_transf) / len(y)
     proportions = np.bincount(y) / len(y)
     return 1 - np.sum(proportions ** 2)
