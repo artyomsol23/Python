80a4c9e0ce513ef6f1a377eeccbc67cfe9055342
Artem S. <artyom.sol23@icloud.com>
2025-08-03 15:46:19 +0300

    Update 09_Q_margin<0.py

--- ml/selfedu-rus_machine_learning/homework/09_Q_margin<0.py
+++ ml/selfedu-rus_machine_learning/homework/09_Q_margin<0.py
@@ -6,7 +6,9 @@ w = np.array([-8/3, -2/3, 1])
 
 # здесь продолжайте программу
 x_test = np.column_stack((np.ones(len(x_test)), x_test))
+
 margin = y_test * (x_test @ w)
+
 Q = np.sum(margin < 0)
 
 print(Q)
