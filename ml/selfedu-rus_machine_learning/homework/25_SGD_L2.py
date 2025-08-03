34a242a31dd5774d1cfb96a2243ba1997600733b
Artem S. <artyom.sol23@icloud.com>
2025-08-03 15:54:09 +0300

    Update 25_SGD_L2.py

--- ml/selfedu-rus_machine_learning/homework/25_SGD_L2.py
+++ ml/selfedu-rus_machine_learning/homework/25_SGD_L2.py
@@ -8,6 +8,7 @@ def func(x):
 # модель
 def model(w, x):
     xv = np.array([x ** n for n in range(len(w))])
+    
     return w.T @ xv
 
 
@@ -19,6 +20,7 @@ def loss(w, x, y):
 # производная функции потерь
 def dL(w, x, y):
     xv = np.array([x ** n for n in range(len(w))])
+    
     return 2 * (model(w, x) - y) * xv
 
 
@@ -58,6 +60,7 @@ for _ in range(n_iter):
     
     w_l2 = np.copy(w)
     w_l2[0] = 0
+    
     w = w - eta * (grad + lm_l2 * w_l2)
 
 Q = np.mean([loss(w, x, y) for x, y in zip(coord_x, coord_y)])
