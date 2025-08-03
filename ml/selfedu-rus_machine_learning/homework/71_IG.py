a7d28724f055256c56e3c8a388513154cd829e6f
Artem S. <artyom.sol23@icloud.com>
2025-08-03 16:16:42 +0300

    Update 71_IG.py

--- ml/selfedu-rus_machine_learning/homework/71_IG.py
+++ ml/selfedu-rus_machine_learning/homework/71_IG.py
@@ -6,7 +6,6 @@ X = np.random.randint(0, 2, size=200)
 # здесь продолжайте программу
 t = 150
 X_left, X_right = X[:t], X[t:]
-
 P0, P1 = np.mean(X == 0), np.mean(X == 1)
 P0_left, P1_left = np.mean(X_left == 0), np.mean(X_left == 1)
 P0_right, P1_right = np.mean(X_right == 0), np.mean(X_right == 1)
