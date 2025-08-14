ccbbdad4df356e6466d49b31aae1de55a8dc44c5
Artem S. <artyom.sol23@icloud.com>
2025-08-14 17:55:36 +0300

    Update 62_backward_Q.py

--- DL/selfedu-rus_neuro-pytorch/homework/62_backward_Q.py
+++ DL/selfedu-rus_neuro-pytorch/homework/62_backward_Q.py
@@ -13,10 +13,10 @@ x0, x1, x2, x3 = map(float, input().split())  # переменные x0, x1, x2,
 
 coords_x = torch.arange(-4, 6, 0.1)  # точки интервала [-4; 6) с шагом 0.1 (тензор в программе не менять)
 
-w0 = torch.tensor(x0, dtype=torch.float32, requires_grad=True)
-w1 = torch.tensor(x1, dtype=torch.float32, requires_grad=True)
-w2 = torch.tensor(x2, dtype=torch.float32, requires_grad=True)
-w3 = torch.tensor(x3, dtype=torch.float32, requires_grad=True)
+w0 = torch.tensor([x0], dtype=torch.float32, requires_grad=True)
+w1 = torch.tensor([x1], dtype=torch.float32, requires_grad=True)
+w2 = torch.tensor([x2], dtype=torch.float32, requires_grad=True)
+w3 = torch.tensor([x3], dtype=torch.float32, requires_grad=True)
 
 Q = 0
 
