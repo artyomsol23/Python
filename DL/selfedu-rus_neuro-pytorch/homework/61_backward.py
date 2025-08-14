607c683b90914ff19c1c766750ad7295f9e9c497
Artem S. <artyom.sol23@icloud.com>
2025-08-14 17:55:18 +0300

    Update 61_backward.py

--- DL/selfedu-rus_neuro-pytorch/homework/61_backward.py
+++ DL/selfedu-rus_neuro-pytorch/homework/61_backward.py
@@ -14,10 +14,10 @@ x = torch.tensor(xx, dtype=torch.float32)
 
 x0, x1, x2, x3 = map(float, input().split()) # переменные x0, x1, x2, x3 в программе не менять
 
-w0 = torch.tensor(x0, dtype=torch.float32, requires_grad=True)
-w1 = torch.tensor(x1, dtype=torch.float32, requires_grad=True)
-w2 = torch.tensor(x2, dtype=torch.float32, requires_grad=True)
-w3 = torch.tensor(x3, dtype=torch.float32, requires_grad=True)
+w0 = torch.tensor([x0], dtype=torch.float32, requires_grad=True)
+w1 = torch.tensor([x1], dtype=torch.float32, requires_grad=True)
+w2 = torch.tensor([x2], dtype=torch.float32, requires_grad=True)
+w3 = torch.tensor([x3], dtype=torch.float32, requires_grad=True)
 
 L = (model(w0, w1, w2, w3, x) - func(x)) ** 2
 L.backward()
