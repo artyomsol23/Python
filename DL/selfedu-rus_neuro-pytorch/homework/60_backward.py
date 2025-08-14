acc8c4871c6381c9c89ae9d018579e6c48452ae0
Artem S. <artyom.sol23@icloud.com>
2025-08-14 17:54:36 +0300

    Update 60_backward.py

--- DL/selfedu-rus_neuro-pytorch/homework/60_backward.py
+++ DL/selfedu-rus_neuro-pytorch/homework/60_backward.py
@@ -4,8 +4,8 @@ import torch
 g = float(input()) # значение g в программе не менять
 d = float(input()) # значение d в программе не менять
 
-G = torch.tensor(g, dtype=torch.float32)
-t = torch.tensor(d, dtype=torch.float32, requires_grad=True)
+G = torch.tensor([g], dtype=torch.float32)
+t = torch.tensor([d], dtype=torch.float32, requires_grad=True)
 
 v = -G * t ** 2 / 2 + G * torch.exp(-t) + 1.5
 v.backward()
