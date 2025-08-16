723782096c407d0e4bbc3c72f2b0730f77ebfd6f
Artem S. <artyom.sol23@icloud.com>
2025-08-16 22:56:43 +0300

    Create 106_add_module.py

---
+++ DL/selfedu-rus_neuro-pytorch/homework/106_add_module.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+batch_size = 16
+x = torch.rand(batch_size, 12) # тензор x в программе не менять
+
+# здесь продолжайте программу
+model = nn.Sequential()
+model.add_module('layer1', nn.Linear(12, 24, bias=True))
+model.add_module('act1', nn.Tanh())
+model.add_module('layer2', nn.Linear(24, 10, bias=True))
+model.add_module('act2', nn.Tanh())
+model.add_module('out', nn.Linear(10, 1, bias=True))
+
+model.eval()
+
+predict = model(x)
