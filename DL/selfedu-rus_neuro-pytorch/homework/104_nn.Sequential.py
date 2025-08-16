abb8151d4238267bb98cc63e661836b52e952c18
Artem S. <artyom.sol23@icloud.com>
2025-08-16 22:50:26 +0300

    Create 104_nn.Sequential.py

---
+++ DL/selfedu-rus_neuro-pytorch/homework/104_nn.Sequential.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+
+batch_size = 8
+x = torch.rand(batch_size, 5)  # тензор x в программе не менять
+
+# здесь продолжайте программу
+model = nn.Sequential(
+    nn.Linear(5, 16, bias=False),
+    nn.ReLU(),
+    nn.BatchNorm1d(16),
+    nn.Linear(16, 3, bias=True)
+)
+
+model.eval()
+
+predict = model(x)
