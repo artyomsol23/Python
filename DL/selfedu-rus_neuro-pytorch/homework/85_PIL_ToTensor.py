dd0b353d505cccf6713b6bdff0842b1150f218ed
Artem S. <artyom.sol23@icloud.com>
2025-08-16 16:11:49 +0300

    Update 85_PIL_ToTensor.py

--- DL/selfedu-rus_neuro-pytorch/homework/85_PIL_ToTensor.py
+++ DL/selfedu-rus_neuro-pytorch/homework/85_PIL_ToTensor.py
@@ -1,11 +1,11 @@
 from PIL import Image
-import torchvision.transforms as tfs
-# import torchvision.transforms.v2 as tfs_v2 - недоступен на Stepik
+import torchvision.transforms.V2 as tfs_v2
+
 
 img_pil = Image.new(mode="RGB", size=(128, 128), color=(0, 128, 255))
 
 # здесь продолжайте программу
-img = tfs.ToTensor()
+img = tfs_v2.ToTensor()
 img = img(img_pil)
 
-# img = tfs.ToTensor()(img_pil)
+# img = tfs_v2.ToTensor()(img_pil)
