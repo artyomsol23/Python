58b3cc64a5acc3e8094cacfc7de85467b044325e
Artem S. <artyom.sol23@icloud.com>
2025-08-16 23:03:04 +0300

    Create 109_nn.Sequential_out1_out2.py

---
+++ DL/selfedu-rus_neuro-pytorch/homework/109_nn.Sequential_out1_out2.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+
+# здесь объявляйте класс модели
+class MyModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.inp = nn.Sequential(
+            nn.Linear(32, 64, bias=False),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(64)
+        )
+        self.out_1 = nn.Sequential(
+            nn.Linear(64, 10, bias=True),
+            nn.Sigmoid()
+        )
+        self.out_2 = nn.Sequential(
+            nn.Linear(64, 32, bias=True),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.inp(x)
+        y = self.out_1(x)
+        t = self.out_2(x)
+        
+        return y, t
+
+
+batch_size=28
+x = torch.rand(batch_size, 32)  # тензор x в программе не менять
+
+# здесь продолжайте программу
+model = MyModel()
+
+model.eval()
+
+predict_y, predict_t = model(x)
