6b9e7a2d7f1eed02ba93e1c5a4433d6d446de536
Artem S. <artyom.sol23@icloud.com>
2025-08-18 17:33:53 +0300

    Update 153_Embedding.py

--- DL/selfedu-rus_neuro-pytorch/homework/153_Embedding.py
+++ DL/selfedu-rus_neuro-pytorch/homework/153_Embedding.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn as nn
 
 
-embedding_layer = nn.Embedding(128, 16)
-embedding_layer.load_state_dict(st_emb)
+emb_layer = nn.Embedding(128, 16)
+emb_layer.load_state_dict(st_emb)
 
 num = torch.LongTensor([78])
 emb_vect = emb_layer(num)
