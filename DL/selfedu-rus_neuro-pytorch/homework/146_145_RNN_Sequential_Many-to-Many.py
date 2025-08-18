db9e5965e0ab086913308a098711dc7001a7ccf1
Artem S. <artyom.sol23@icloud.com>
2025-08-18 14:57:20 +0300

    Create 146_145_RNN_Sequential_Many-to-Many.py

---
+++ DL/selfedu-rus_neuro-pytorch/homework/146_145_RNN_Sequential_Many-to-Many.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+
+
+# здесь объявляйте класс OutputModule
+class OutputModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.act = nn.ReLU(inplace=True)
+        self.layer = nn.Linear(25, 10, bias=True)
+
+    def forward(self, x):
+        return self.layer(self.act(x[0]))
+
+
+# тензор x в программе не менять
+batch_size = 7
+seq_length = 5
+in_features = 15
+
+x = torch.rand(batch_size, seq_length, in_features)
+
+# здесь продолжайте программу
+model = nn.Sequential(
+    nn.RNN(input_size=in_features, hidden_size=25, batch_first=True),
+    OutputModule()
+)
+
+model.eval()
+
+out = model(x)
