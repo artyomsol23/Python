8308732af452122a9ab1b3dde6f9579fe111beb2
Artem S. <artyom.sol23@icloud.com>
2025-08-19 15:40:53 +0300

    Create 156_RNN_bidirectional.py

---
+++ DL/selfedu-rus_neuro-pytorch/homework/156_RNN_bidirectional.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+
+
+# здесь объявляйте класс модели
+class RNNModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.rnn = nn.RNN(
+            input_size=32,
+            hidden_size=12,
+            num_layers=1,
+            nonlinearity='tanh',
+            bias=True, 
+            batch_first=True,
+            dropout=0,
+            bidirectional=True,
+        )
+        self.out = nn.Linear(
+            in_features=24,
+            out_features=5,
+            bias=True
+        )
+
+    def forward(self, x):
+        x, h = self.rnn(x)
+        y = torch.cat([h[0], h[1]], dim=1)
+        
+        return self.out(y)
+
+
+batch_size = 8
+seq_length = 12
+d_size = 32
+
+x = torch.rand(batch_size, seq_length, d_size)
+
+model = RNNModel() # создание объекта модели
+
+# здесь продолжайте программу
+model.eval()
+
+predict = model(x)
