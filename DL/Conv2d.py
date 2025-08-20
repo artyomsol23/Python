f6c780e3295c9752e75efddc7571a4fec9d758ee
Artem S. <artyom.sol23@icloud.com>
2025-08-20 15:01:29 +0300

    Create Conv2d.py

---
+++ DL/Conv2d.py
@@ -0,0 +1,13 @@
+torch.nn.Conv2d(
+    in_channels=, 
+    out_channels=, 
+    kernel_size=, 
+    stride=1, 
+    padding=0, 
+    dilation=1, 
+    groups=1, 
+    bias=True, 
+    padding_mode='zeros', 
+    device=None, 
+    dtype=None
+)
