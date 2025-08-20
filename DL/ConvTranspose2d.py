732ea691bcfd94fc74ca982c77128224c4081fd6
Artem S. <artyom.sol23@icloud.com>
2025-08-20 15:11:10 +0300

    Create ConvTranspose2d.py

---
+++ DL/ConvTranspose2d.py
@@ -0,0 +1,14 @@
+nn.ConvTranspose2d(
+    in_channels=,
+    out_channels=,
+    kernel_size=,
+    stride=1,
+    padding=0,
+    output_padding=0,
+    groups=1,
+    bias=True,
+    dilation=1,
+    padding_mode='zeros',
+    device=None,
+    dtype=None
+)
